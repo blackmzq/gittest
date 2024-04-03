238bddad9b2d735cd36c426f34e5ecc5b03b3edc
qiji <144003152+blackmzq@users.noreply.github.com>
2024-04-03 15:53:18 +0800

    Add files via upload

---
+++ course2.java
@@ -0,0 +1,18 @@
+package course;
+public class course{
+public static void main(String[] args) {
+System.out.println("helloworld");
+}
+public class Count{
+    private int serialNumber;
+    private static int counter=0;
+    public Count(){
+        counter++;
+        serialNumber=cuonter;
+        system.out.print(serialNumber);
+    }
+    public class Test{
+        Count c1=new Count();
+        Count c2=new Count();
+    }
+}
